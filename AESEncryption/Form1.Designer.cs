12cdc224681b5f52a8bafb62c1b12041602986ba
dothangg <dothangg0503@gmail.com>
2024-04-08 14:10:10 +0700

    done ase client socket

---
+++ AESEncryption/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace AESEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonExit = new System.Windows.Forms.Button();
+            groupBox2 = new System.Windows.Forms.GroupBox();
+            textIP = new System.Windows.Forms.TextBox();
+            label6 = new System.Windows.Forms.Label();
+            button3 = new System.Windows.Forms.Button();
+            label3 = new System.Windows.Forms.Label();
+            textBoxDcryptPassword = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            textBoxEncrypted = new System.Windows.Forms.TextBox();
+            buttonDecrypt = new System.Windows.Forms.Button();
+            groupBox3 = new System.Windows.Forms.GroupBox();
+            textBoxDebug = new System.Windows.Forms.TextBox();
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            button5 = new System.Windows.Forms.Button();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            buttonExit.Enabled = false;
+            buttonExit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            buttonExit.Location = new System.Drawing.Point(449, 288);
+            buttonExit.Name = "buttonExit";
+            buttonExit.Size = new System.Drawing.Size(94, 29);
+            buttonExit.TabIndex = 2;
+            buttonExit.Text = "Receive";
+            buttonExit.UseVisualStyleBackColor = true;
+            buttonExit.Click += buttonExit_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textIP);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(textBoxDcryptPassword);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBoxEncrypted);
+            groupBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            groupBox2.Location = new System.Drawing.Point(12, 36);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new System.Drawing.Size(775, 229);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Decryption";
+            // 
+            // textIP
+            // 
+            textIP.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textIP.Location = new System.Drawing.Point(9, 184);
+            textIP.Name = "textIP";
+            textIP.Size = new System.Drawing.Size(390, 27);
+            textIP.TabIndex = 9;
+            textIP.TextChanged += textIP_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label6.Location = new System.Drawing.Point(11, 161);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(123, 20);
+            label6.TabIndex = 12;
+            label6.Text = "Server IP Address";
+            label6.Click += label6_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(568, 48);
+            button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(115, 31);
+            button3.TabIndex = 7;
+            button3.Text = "Decrypt file";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(8, 93);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(336, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Provide password to use for decryption (16 chars)";
+            // 
+            // textBoxDcryptPassword
+            // 
+            textBoxDcryptPassword.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBoxDcryptPassword.Location = new System.Drawing.Point(8, 117);
+            textBoxDcryptPassword.Name = "textBoxDcryptPassword";
+            textBoxDcryptPassword.Size = new System.Drawing.Size(390, 27);
+            textBoxDcryptPassword.TabIndex = 3;
+            textBoxDcryptPassword.TextChanged += textBoxDcryptPassword_TextChanged;
+            textBoxDcryptPassword.MouseDoubleClick += textBoxDcryptPassword_MouseDoubleClick;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label4.Location = new System.Drawing.Point(8, 25);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(244, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Enter encrypted text (base64 string)";
+            // 
+            // textBoxEncrypted
+            // 
+            textBoxEncrypted.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            textBoxEncrypted.Location = new System.Drawing.Point(8, 48);
+            textBoxEncrypted.Name = "textBoxEncrypted";
+            textBoxEncrypted.Size = new System.Drawing.Size(523, 27);
+            textBoxEncrypted.TabIndex = 0;
+            textBoxEncrypted.TextChanged += textBoxEncrypted_TextChanged;
+            textBoxEncrypted.MouseDoubleClick += textBoxEncrypted_MouseDoubleClick;
+            // 
+            // buttonDecrypt
+            // 
+            buttonDecrypt.Enabled = false;
+            buttonDecrypt.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            buttonDecrypt.Location = new System.Drawing.Point(592, 288);
+            buttonDecrypt.Name = "buttonDecrypt";
+            buttonDecrypt.Size = new System.Drawing.Size(94, 29);
+            buttonDecrypt.TabIndex = 7;
+            buttonDecrypt.Text = "Decrypt";
+            buttonDecrypt.UseVisualStyleBackColor = true;
+            buttonDecrypt.Click += buttonDecrypt_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBoxDebug);
+            groupBox3.Location = new System.Drawing.Point(13, 350);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new System.Drawing.Size(775, 77);
+            groupBox3.TabIndex = 13;
+            groupBox3.TabStop = false;
+            // 
+            // textBoxDebug
+            // 
+            textBoxDebug.Location = new System.Drawing.Point(10, 35);
+            textBoxDebug.Name = "textBoxDebug";
+            textBoxDebug.ReadOnly = true;
+            textBoxDebug.Size = new System.Drawing.Size(747, 27);
+            textBoxDebug.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button5
+            // 
+            button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            button5.Location = new System.Drawing.Point(316, 288);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(94, 29);
+            button5.TabIndex = 14;
+            button5.Text = "Clear";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 441);
+            Controls.Add(button5);
+            Controls.Add(groupBox3);
+            Controls.Add(buttonDecrypt);
+            Controls.Add(groupBox2);
+            Controls.Add(buttonExit);
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Decryption AES";
+            Load += Form1_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDcryptPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxDebug;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textIP;
+    }
+}
+
---
+++ AESEncryption/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net.Http;
+using AESEncryption.Model;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+using System.Reflection.Metadata;
+
+namespace AESEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            textBoxDcryptPassword.Text = ReceiveSocketKey(textIP.Text);
+
+            WriteBytesToFile("_encrypted.txt", ReceiveSocketData(textIP.Text));
+        }
+
+
+        static byte[] ReceiveSocketData(string ip)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                // 1. connect
+                client.Connect(ip, 9999);
+                NetworkStream stream = client.GetStream();
+
+                // Create a buffer to store the received data
+                byte[] buffer = new byte[1024]; // Adjust buffer size as needed
+
+                Console.WriteLine("Connected to Y2Server.");
+
+                // Read data from the stream
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int bytesRead;
+                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, bytesRead);
+                    }
+
+                    // Convert the accumulated bytes to a byte array
+                    return ms.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return null;
+            }
+        }
+        static string ReceiveSocketKey(string ip)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                // 1. connect
+                client.Connect(ip, 8888);
+                Stream stream = client.GetStream();
+                string result = "";
+                Console.WriteLine("Connected to Y2Server.");
+                while (true)
+                {
+
+                    var reader = new StreamReader(stream);
+                    string response;
+
+                    while ((response = reader.ReadLine()) != null)
+                    {
+                        result += response;
+                    }
+
+                    break;
+                }
+
+                // 4. close
+                stream.Close();
+                client.Close();
+                return result;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+                return "";
+            }
+        }
+
+
+        static void WriteBytesToFile(string filePath, byte[] data)
+        {
+            try
+            {
+                // Mở hoặc tạo file mới
+                using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    fs.SetLength(0);
+                    // Ghi tất cả byte vào file
+                    fs.Write(data, 0, data.Length);
+                    fs.Flush(); // Đảm bảo dữ liệu được ghi ngay lập tức
+                }
+
+                // Kiểm tra xem file đã được tạo hay chưa
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine("File created successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to create file.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+            }
+        }
+
+
+        static void DecryptReceivedFile(string filePath, string key)
+        {
+            // Xóa nội dung của file giải mã và ghi dữ liệu mới vào file
+            //using (FileStream fsEncrypted = new FileStream(filePath, FileMode.Open))
+
+            AES.Decrypt(filePath, key);
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //var data = Decrypt(textBoxEncrypted.Text, textBoxDcryptPassword.Text);
+                //textBoxDecryptOutput.Text = data.StringEncryptOrDecrypt;
+                //textTimeDecrypt.Text = data.DecrypEncryptTime.ToString("0.#############");
+                DecryptReceivedFile(textBoxEncrypted.Text, textBoxDcryptPassword.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBoxInput_TextChanged(object sender, EventArgs e)
+        {
+            //textBoxEncryptPassword.Enabled = textBoxInput.Text.Length > 0;
+            //buttonEncrypt.Enabled = textBoxInput.Text.Length > 0;
+        }
+
+        private void textBoxEncryptPassword_TextChanged(object sender, EventArgs e)
+        {
+            //textBoxInput.Enabled = textBoxInput.Text.Length > 0;
+            //buttonEncrypt.Enabled = textBoxInput.Text.Length > 0;
+
+        }
+
+        private void textBoxEncrypted_TextChanged(object sender, EventArgs e)
+        {
+            textBoxDcryptPassword.Enabled = textBoxEncrypted.Text.Length > 0;
+            buttonDecrypt.Enabled = textBoxEncrypted.Text.Length > 0;
+        }
+
+        private void textBoxDcryptPassword_TextChanged(object sender, EventArgs e)
+        {
+            textBoxEncrypted.Enabled = textBoxEncrypted.Text.Length > 0;
+            buttonDecrypt.Enabled = textBoxEncrypted.Text.Length > 0;
+        }
+
+
+
+        private void textBoxEncryptPassword_Leave(object sender, EventArgs e)
+        {
+            if (((TextBox)sender).Text.Length != 16)
+            {
+                // MessageBox.Show("You need to write at least 16 characters.");
+            }
+        }
+
+        private void textBoxInput_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //if (textBoxInput.Text.Length > 0)
+            //{
+            //    byte[] InputBytes = Encoding.UTF8.GetBytes(textBoxInput.Text);
+            //    textBoxDebug.Text = BitConverter.ToString(InputBytes).ToLower().Replace("-", " ");
+            //}
+        }
+
+        private void textBoxEncryptPassword_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //if (textBoxEncryptPassword.Text.Length > 0)
+            //{
+            //    byte[] InputBytes = Encoding.UTF8.GetBytes(textBoxEncryptPassword.Text);
+            //    textBoxDebug.Text = BitConverter.ToString(InputBytes).ToLower().Replace("-", " ");
+            //}
+        }
+
+        private void textBoxEncryptedOutput_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //if (textBoxEncryptedOutput.Text.Length > 0)
+            //{
+            //    byte[] InputBytes = Convert.FromBase64String(textBoxEncryptedOutput.Text);
+            //    textBoxDebug.Text = BitConverter.ToString(InputBytes).ToLower().Replace("-", " ");
+            //}
+        }
+
+        private void textBoxEncrypted_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            byte[] InputBytes = Convert.FromBase64String(textBoxEncrypted.Text);
+            textBoxDebug.Text = BitConverter.ToString(InputBytes).ToLower().Replace("-", " ");
+        }
+
+        private void textBoxDcryptPassword_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (textBoxDcryptPassword.Text.Length > 0)
+            {
+                byte[] InputBytes = Encoding.UTF8.GetBytes(textBoxDcryptPassword.Text);
+                textBoxDebug.Text = BitConverter.ToString(InputBytes).ToLower().Replace("-", "");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFile.FileName))
+                    {
+                        string content = sr.ReadToEnd();
+                        //textBoxInput.Text = content;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            string randomString = "";
+
+            int length = 16;
+
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (int i = 0; i < length; i++)
+            {
+                randomString += chars[random.Next(chars.Length)];
+            }
+            //textBoxEncryptPassword.Text = randomString;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    textBoxEncrypted.Text = openFile.FileName;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            };
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            //textBoxDecryptOutput.Text = "";
+            textBoxEncrypted.Text = "";
+            textIP.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFile.FileName))
+                    {
+                        string content = sr.ReadToEnd();
+                        textBoxDcryptPassword.Text = content;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxEncryptedOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textIP_TextChanged(object sender, EventArgs e)
+        {
+            
+            buttonExit.Enabled = textIP.Text.Length > 0;
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
